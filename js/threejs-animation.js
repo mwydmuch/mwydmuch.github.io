8704534ecd0a092de7a8bb9a2e845ce7ad205eb3
Marek Wydmuch <marek@wydmuch.poznan.pl>
2024-07-23 10:30:48 +0200

    Add test three.js animation

--- js/threejs-animation.js
+++ js/threejs-animation.js
@@ -1,9 +1,8 @@
 'use strict';
 
 /*
- * Base class for all the background animations.
+ * Base class for the background animations that use three.js
  */
-
 
 const Animation = require("./animation");
 
@@ -13,13 +12,38 @@ class ThreejsAnimation extends Animation {
                 file = "",
                 description = "",
                 seed = "random") {
-        super(canvas, colors, colorsAlt, bgColor, name, file, description, seed);
-        renderer = new THREE.WebGLRenderer( { "canvas": canvas } );
+        super(canvas, colors, colorsAlt, bgColor, name, file, description, seed, null);
+
+        // Create a WebGL renderer
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
+
+        // Set the size of the renderer to the size of the window
+        this.renderer.setSize(canvas.width / canvas.height);
         
+        // Create a new scene
+        this.scene = new THREE.Scene();
+
+        // Create a camera, which determines what we'll see when we render the scene
+        this.camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 1, 1000);
+        this.camera.position.set(0, 0, 5);
+
+        //this.renderer.setClearColor(0xFFFFFF);
+    }
+
+    update(elapsed){
+        super.update(elapsed);
     }
 
     draw() {
-        
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    resize(){
+        const width = this.canvas.width,
+              height = this.canvas.height;
+        this.renderer.setSize(width, height);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
     }
 
     getCodeUrl(){
@@ -27,4 +51,4 @@ class ThreejsAnimation extends Animation {
     }
 }
 
-module.exports = ShaderAnimation;
+module.exports = ThreejsAnimation;
